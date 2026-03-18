4c235f390f307df1c5d973073708d985149642b0
JozefPaulisin <jozefpaulisin@gmail.com>
2026-03-18 09:02:58 +0100

    Update payload.config.ts
    fixed imports

--- payload.config.ts
+++ payload.config.ts
@@ -2,29 +2,37 @@ import { buildConfig } from 'payload'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import sharp from 'sharp'
-import { Media } from '@/collections/Media'
-import { TeamMembers } from '@/collections/TeamMembers'
-import { Articles } from '@/collections/Articles'
-import { Partners } from '@/collections/Partners'
-import { Events } from '@/collections/Events'
-import { SiteSettings } from '@/globals/SiteSettings'
-import { SupportSettings } from '@/globals/SupportSettings'
-import { BlogSettings } from '@/globals/BlogSettings'
+
+//  fixed imports (without @/)
+import { Media } from './collections/Media'
+import { TeamMembers } from './collections/TeamMembers'
+import { Articles } from './collections/Articles'
+import { Partners } from './collections/Partners'
+import { Events } from './collections/Events'
+
+import { SiteSettings } from './globals/SiteSettings'
+import { SupportSettings } from './globals/SupportSettings'
+import { BlogSettings } from './globals/BlogSettings'
 
 export default buildConfig({
-    editor: lexicalEditor(),
-    collections: [Media, TeamMembers, Articles, Partners, Events],
-    globals: [SiteSettings, SupportSettings, BlogSettings],
-    secret: process.env.PAYLOAD_SECRET || 'fallback-secret-for-dev',
-    db: postgresAdapter({
-        pool: {
-                  connectionString: process.env.DATABASE_URL,
-                },
-        migrationDir: './migrations',
-        push: true,
-    }),
-    sharp,
-    graphQL: {
-        disable: true,
+  editor: lexicalEditor(),
+
+  collections: [Media, TeamMembers, Articles, Partners, Events],
+  globals: [SiteSettings, SupportSettings, BlogSettings],
+
+  secret: process.env.PAYLOAD_SECRET || 'fallback-secret-for-dev',
+
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URL,
     },
+    migrationDir: './migrations',
+    push: true,
+  }),
+
+  sharp,
+
+  graphQL: {
+    disable: true,
+  },
 })
